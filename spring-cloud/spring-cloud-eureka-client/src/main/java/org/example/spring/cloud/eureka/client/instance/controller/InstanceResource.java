d1a6965fb3eef851a2d3d592ae240862de5298ff
조승열 <sycho@spectra.co.kr>
2022-12-22 08:50:20 +0900

    add spring cloud eureka example

---
+++ spring-cloud/spring-cloud-eureka-client/src/main/java/org/example/spring/cloud/eureka/client/instance/controller/InstanceResource.java
@@ -0,0 +1,24 @@
+package org.example.spring.cloud.eureka.client.instance.controller;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/instances")
+public class InstanceResource {
+
+    private final DiscoveryClient discoveryClient;
+
+    @GetMapping("{applicationName}")
+    public List<ServiceInstance> findAll(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
