58ed73b2131c5c3876d53a6061d5231f0a3523e0
조승열 <sycho@spectra.co.kr>
2022-12-21 13:53:03 +0900

    modify sample data

--- spring-cache/spring-cache-caffeine/src/main/java/org/example/spring/cache/caffeine/user/sample/UserSample.java
+++ spring-cache/spring-cache-caffeine/src/main/java/org/example/spring/cache/caffeine/user/sample/UserSample.java
@@ -5,18 +5,17 @@ import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.example.spring.cache.caffeine.user.domain.User;
 import org.example.spring.cache.caffeine.user.repository.UserRepository;
-import org.springframework.context.event.ContextStartedEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class UserSample {
+public class UserSample implements CommandLineRunner {
 
     private final UserRepository userRepository;
 
-    @EventListener(classes = ContextStartedEvent.class)
-    public void run() {
+    @Override
+    public void run(String... args) {
         Stream.of(
             new User(1L, "user01", "user01@gamil.com"),
             new User(2L, "user02", "user02@gamil.com"),
