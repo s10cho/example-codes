ffb4169da8f5ba9d7ddf174bc47425b5c1b72f5e
조승열 <sycho@spectra.co.kr>
2023-03-21 15:48:48 +0900

    fix test code

--- spring-sample/spring-scheduling-alarm-keeper/src/test/java/com/spring/sample/schedule/alarm/keeper/service/NotificationServiceTest.java
+++ spring-sample/spring-scheduling-alarm-keeper/src/test/java/com/spring/sample/schedule/alarm/keeper/service/NotificationServiceTest.java
@@ -1,5 +1,10 @@
 package com.spring.sample.schedule.alarm.keeper.service;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +38,14 @@ class NotificationServiceTest {
         service.cancelNotification("4");
 
         Thread.sleep(15 * 1000);
+    }
+
+    @Test
+    void timeTest() {
+        LocalTime now = LocalTime.now();
+        LocalTime alarmTime = now.plus(5, ChronoUnit.SECONDS);
+        long delaySeconds = Duration.between(now, alarmTime).toSeconds();
+
+        log.debug("delaySeconds: {}", delaySeconds);
     }
 }
